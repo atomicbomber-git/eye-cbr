7a3ca79b49bfb598cd8fbcdd79387a93ee18ba20
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-02-18 20:59:41 +0700

    Navbar now used app name instead of hardcoded string.

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -6,7 +6,7 @@
                     <i class="fa fa-icon"></i>
                 </span>
                 <span>
-                    Eye CBR
+                    {{ config('app.name') }}
                 </span>
             </a>
 
