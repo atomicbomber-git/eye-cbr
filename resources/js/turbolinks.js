fae2fa89a45011a96c57d213edde283225f934e4
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-02-18 23:43:55 +0700

    Some fixed to adjust to Turbolinks.

---
+++ resources/js/turbolinks.js
@@ -0,0 +1,5 @@
+// Turbolinks
+const Turbolinks = require('turbolinks')
+Turbolinks.start()
+
+console.log("TEST")
--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -7,6 +7,7 @@
     <title> {{ config('app.name') }} </title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/turbolinks.js') }}"></script>
 </head>
 <body>
     @include('shared.navbar')
